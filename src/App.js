23372eb42561a2f64cae1d452b6cbaa524df7490
nokiusz <mati.klakus@gmail.com>
2021-08-06 16:41:57 +0200

    cleaned code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Game from './components/js/Game';
+import Links from './components/js/Links.js';
+import './main.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Conway's Game of Life</h1>
+      <Game />
+      <Links />
+    </>
   );
-}
+};
 
 export default App;
