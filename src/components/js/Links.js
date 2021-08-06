865554ced90ad10b3b871d6f9110905fba12a897
nokiusz <mati.klakus@gmail.com>
2021-08-06 16:50:30 +0200

    Links component

--- src/components/js/Links.js
+++ src/components/js/Links.js
@@ -3,7 +3,7 @@ import '../css/Links.css';
 
 const Links = () => (
   <div className='links'>
-    <a href='https://github.com/Nokiusz'>repo</a>
+    <a href='https://github.com/Nokiusz/GAME-OF-LIFE'>repo</a>
     <a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'>wiki</a>
   </div>
 );
